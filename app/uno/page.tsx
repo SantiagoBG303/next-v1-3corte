58c82be08b85b254bee51708dbc18b7d320ed2f8
santiago <santiago.bermudezg@uniagustiniana.edu.co>
2026-05-08 18:59:01 +0000

    8 de mayo

---
+++ app/uno/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Props from './props';
+
+
+export default function Uno() {
+  
+
+  return (
+    <div>
+      uno
+
+      <Props nombre="este es un props" />
+    
+    </div>
+  );
+}
